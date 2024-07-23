364041145cc1eb6f7939091c0c83855ca80632a9
Diogo Sanlez <diogosdl25@gmail.com>
2024-07-23 16:52:40 +0100

    SeedDb

---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,46 @@
+﻿using SuperShop.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if(!_context.Products.Any()) 
+            {
+                AddProduct("Iphone X");
+                AddProduct("Iphone XV");
+                AddProduct("Ipad Mini");
+                AddProduct("SmartWatch");
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name  = name,
+                Price = _random.Next(1000),
+                IsAvaible = true,
+                Stock = _random.Next(100)
+
+            });
+        }
+    }
+}
