22e3a278c2592c32c1fb1e70d6b6abe10c54ea62
Diogo Sanlez <diogosdl25@gmail.com>
2024-09-09 15:59:42 +0100

    Create Token

--- SuperShop/Helpers/IUserHelper.cs
+++ SuperShop/Helpers/IUserHelper.cs
@@ -12,13 +12,21 @@ namespace SuperShop.Helpers
         Task<IdentityResult> AddUserAsync(User user, string password);
 
         Task<SignInResult> LoginAsync(LoginViewModel model);
+
         Task LogoutAsync();
 
         Task<IdentityResult> UpdateUserAsync(User user);
 
         Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
         Task CheckRoleAsync(string roleName);
+
         Task AddUserToRoleAsync(User user, string roleName);
+
         Task <bool>IsUserInRoleAsync(User user, string roleName);
+
+        Task<SignInResult> ValidatePasswordAsync(User user, string password);
+
+
     }
 }
